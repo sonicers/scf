6fb36dfc85121c1a36b97603cfa9bbe136a295d0
yourongcan <iponkan@gmail.com>
2020-02-26 22:50:35 +0800

    node尝试

--- get_world/index.js
+++ get_world/index.js
@@ -1,10 +1,16 @@
 'use strict'
 
-// 云函数入口函数
-/**
- * 拉取微信access_token
- */
+const zmq = require("zeromq")
+
 exports.main_handler = async (event, context) => {
-    console.log("event==========", event)
-    return "world"
+
+sock = zmq.socket("push");
+
+sock.bindSync("tcp://127.0.0.1:3000");
+console.log("Producer bound to port 3000");
+
+setInterval(function() {
+    console.log("sending work");
+    sock.send("some work");
+    }, 500);
 }
